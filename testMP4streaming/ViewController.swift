07afe552fe54c96086cf1d1352c0ea520c9364d3
QoT <vedrano@vedrans-mini.lan>
2016-04-10 20:09:27 +0200

    Initial Commit

---
+++ testMP4streaming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testMP4streaming
+//
+//  Created by Vedran Ozir on 10/04/16.
+//  Copyright © 2016 Vedran Ozir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
